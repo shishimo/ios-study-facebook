f1617d70c17dbbec12ab8b6d1ecafa341f2a3717
shishimo <my_blue_wish@yahoo.co.jp>
2018-12-02 14:25:44 +0700

    created a new project.

---
+++ ios-study-facebook/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ios-study-facebook
+//
+//  Created by Shinichiro Shimoda on 2018/12/02.
+//  Copyright © 2018 Shinichiro Shimoda. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
